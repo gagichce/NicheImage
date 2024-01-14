f832c99aa23462337e8128a2955e53ca7ab01612
minh132 <trinhxuanminh1322002@gmail.com>
2024-01-14 10:29:15 +0000

    update version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ with codecs.open(
 
 setup(
     name="nicheimage",
-    version="0.0.5",
+    version="0.0.6",
     description="NicheImage - Subnet 23 - Bittensor",
     long_description=long_description,
     long_description_content_type="text/markdown",
